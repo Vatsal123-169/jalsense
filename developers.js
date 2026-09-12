30b6fabe080563aaa905f37cedf4d2a5ebbf7ec4
Vatsal123-169 <dixitvatsal882@gmail.com>
2026-09-12 19:06:09 +0530

    Create developers.js

---
+++ developers.js
@@ -0,0 +1,27 @@
+const cards = document.querySelectorAll(".developer-card");
+
+cards.forEach((card) => {
+
+    card.addEventListener("mousemove", (e) => {
+
+        const rect = card.getBoundingClientRect();
+
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        const rotateX = ((y / rect.height) - 0.5) * -4;
+        const rotateY = ((x / rect.width) - 0.5) * 4;
+
+        card.style.transform =
+            `perspective(900px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateY(-8px)`;
+
+    });
+
+    card.addEventListener("mouseleave", () => {
+
+        card.style.transform =
+            "perspective(900px) rotateX(0deg) rotateY(0deg) translateY(0)";
+
+    });
+
+});
